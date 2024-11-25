8b5ca0ebb2a8ee54a3ca71eb26dfc5229aef6bd9
opedrock <phklucio@gmail.com>
2024-11-25 20:30:18 -0300

    páginas

--- src/Components/Quadrinhos.tsx
+++ src/Components/Quadrinhos.tsx
@@ -7,7 +7,25 @@ function Quadrinhos() {
 
             <header>
                 <nav id="navbar">
-                    
+                    <i className="logo-nav" id="nav-logo">Heróis</i>
+
+                    <ul id="nav_list">
+                        <li className="nav-item">
+                            <a href="#home">Início</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a href="#menu">Quadrinhos</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a href="#info">Sobre Nós</a>
+                        </li>
+                    </ul>
+
+                    <button className="btn-default">
+                        Ver Novidades
+                    </button>
                 </nav>
             </header>
 
